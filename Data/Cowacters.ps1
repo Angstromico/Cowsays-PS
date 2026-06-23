6e8656a83425cdbf540c1d6721155ae7661f35ec
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-23 10:41:09 -0400

    feat(powershell): define all 45 cowacters in Data/Cowacters.ps1

--- Data/Cowacters.ps1
+++ Data/Cowacters.ps1
@@ -1,1 +1,723 @@
-# Cowacters data placeholder
+# Data/Cowacters.ps1
+
+$Cowacters = @{
+    'beavis' = @{
+        Body = @'
+   {thoughts}         __------~~-,
+    {thoughts}      ,'            ,
+          /               \
+         /                :
+        |                  '
+        |                  |
+        |                  |
+         |   _--           |
+         _| =-.     .-.   ||
+         o|/o/       _.   |
+         /  ~          \ |
+       (____@)  ___~    |
+          |_===~~~.`    |
+       _______.--~     |
+       \________       |
+                \      |
+              __/-___-- -__
+             /            _ \
+'@
+        HasThreeEyes = $false
+    }
+    'bongcow' = @{
+        Body = @'
+         {thoughts}
+          {thoughts}
+            ^__^
+    _______/({eyes})
+/\/(       /(__)
+   | W----|| |~|
+   ||     || |~|  ~~
+             |~|  ~
+             |_| o
+             |#|/
+            _+#+_
+
+'@
+        HasThreeEyes = $false
+    }
+    'budfrogs' = @{
+        Body = @'
+     {thoughts}
+      {thoughts}
+          oO)-.                       .-(Oo
+         /__  _\                     /_  __\
+         \  \(  |     ()~()         |  )/  /
+          \__|\ |    (-___-)        | /|__/
+          '  '--'    ==`-'==        '--'  '
+'@
+        HasThreeEyes = $false
+    }
+    'bunny' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}   \
+        \ /\
+        ( )
+      .( o ).
+'@
+        HasThreeEyes = $false
+    }
+    'cheese' = @{
+        Body = @'
+   {thoughts}
+    {thoughts}
+      _____   _________
+     /     \_/         |
+    |                 ||
+    |                 ||
+   |    ###\  /###   | |
+   |     0  \/  0    | |
+  /|                 | |
+ / |        <        |\ \
+| /|                 | | |
+| |     \_______/   |  | |
+| |                 | / /
+/||                 /|||
+   ----------------|
+        | |    | |
+        ***    ***
+       /___\  /___\
+'@
+        HasThreeEyes = $false
+    }
+    'cower' = @{
+        Body = @'
+     {thoughts}
+      {thoughts}
+        ,__, |    | 
+        (oo)\|    |___
+        (__)\|    |   )\_
+             |    |_w |  \
+             |    |  ||   *
+
+             Cower....
+'@
+        HasThreeEyes = $false
+    }
+    'daemon' = @{
+        Body = @'
+   {thoughts}         ,        ,
+    {thoughts}       /(        )`
+     {thoughts}      \ \___   / |
+            /- _  `-/  '
+           (/\/ \ \   /\
+           / /   | `    \
+           O O   ) /    |
+           `-^--'`<     '
+          (_.)  _  )   /
+           `.___/`    /
+             `-----' /
+<----.     __ / __   \
+<----|====O)))==) \) /====
+<----'    `--' `.__,' \
+             |        |
+              \       /
+        ______( (_  / \______
+      ,'  ,-----'   |        \
+      `--{__________)        \/
+'@
+        HasThreeEyes = $false
+    }
+    'default' = @{
+        Body = @'
+     {thoughts}   ^__^
+      {thoughts}  ({eyes})\_______
+         (__)\       )\/\
+          {tongue} ||----w |
+           ||     ||
+'@
+        HasThreeEyes = $false
+    }
+    'dragonandcow' = @{
+        Body = @'
+             {thoughts}                    ^    /^
+              {thoughts}                  / \  // \
+               {thoughts}   |\___/|      /   \//  .\
+                {thoughts}  /O  O  \__  /    //  | \ \           *----*
+                  /     /  \/_/    //   |  \  \          \   |
+                  @___@``    \/_   //    |   \   \         \/\ \
+                 0/0/|       \/_ //     |    \    \         \ \
+             0/0/0/0/|        \///      |     \     \       | |
+          0/0/0/0/0/_|_ /   (  //       |      \     _\     |  /
+       0/0/0/0/0/0/`/,_ _ _/  ) ; -.    |    _ _\.-~       /   /
+                   ,-}        _      *-.|.-~-.           .~    ~
+  \     \__/        `/\      /                 ~-. _ .-~      /
+   \____({eyes})           *.   }            {                   /
+   (    (--)          .----~-.\        \-`                 .~
+   //__\\  \__ Ack!   ///.----..<        \             _ -~
+  //    \\               ///-._ _ _ _ _ _ _{^ - - - - ~
+'@
+        HasThreeEyes = $false
+    }
+    'eyes' = @{
+        Body = @'
+    {thoughts}
+     {thoughts}
+                                   .::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW`$`$`$ 
+      :`$`$NWX!!:           .:!!!!!!XUWW`$`$`$`$`$`$`$`$`$P 
+      `$`$`$`$`$##WX!:      .<!!!!UW`$`$`$`$"  `$`$`$`$`$`$`$`$#
+      `$`$`$`$`$  `$`$`$UX   :!!UW`$`$`$`$`$`$`$`$`$   4`$`$`$`$`$`* 
+      ^`$`$`$B  `$`$`$`$\     `$`$`$`$`$`$`$`$`$`$`$`$   d`$`$R
+        "*`$bd`$`$`$`$      '*`$`$`$`$`$`$`$`$`$`$`$o+#" 
+             """"          """"""" 
+
+'@
+        HasThreeEyes = $false
+    }
+    'flamingsheep' = @{
+        Body = @'
+  {thoughts}            .    .     .   
+   {thoughts}      .  . .     `  ,     
+    {thoughts}    .; .  : .' :  :  : . 
+     {thoughts}   i..`: i` i.i.,i  i . 
+      {thoughts}   `,--.|i |i|ii|ii|i: 
+           U{eyes}U\.'@@@@@@`.||' 
+           \__/(@@@@@@@@@@)'  
+                (@@@@@@@@)    
+                `YY~~~~YY'    
+                 ||    ||     
+'@
+        HasThreeEyes = $false
+    }
+    'ghostbusters' = @{
+        Body = @'
+          {thoughts}
+           {thoughts}
+            {thoughts}          __---__
+                    _-       /--______
+               __--( /     \ )XXXXXXXXXXX\v.
+             .-XXX(   O   O  )XXXXXXXXXXXXXXX-
+            /XXX(       U     )        XXXXXXX\
+          /XXXXX(              )--_  XXXXXXXXXXX\
+         /XXXXX/ (      O     )   XXXXXX   \XXXXX\
+         XXXXX/   /            XXXXXX   \__ \XXXXX
+         XXXXXX__/          XXXXXX         \__---->
+ ---___  XXX__/          XXXXXX      \__         /
+   \-  --__/   ___/\  XXXXXX            /  ___--/=
+    \-\    ___/    XXXXXX              '--- XXXXXX
+       \-\/XXX\ XXXXXX                      /XXXXX
+         \XXXXXXXXX   \                    /XXXXX/
+          \XXXXXX      >                 _/XXXXX/
+            \XXXXX--__/              __-- XXXX/
+             -XXXXXXXX---------------  XXXXXX-
+                \XXXXXXXXXXXXXXXXXXXXXXXXXX/
+                  ""VXXXXXXXXXXXXXXXXXXV""
+'@
+        HasThreeEyes = $false
+    }
+    'headincow' = @{
+        Body = @'
+    {thoughts}
+     {thoughts}
+    ^__^         /
+    ({eyes})\_______/  _________
+    (__)\       )=(  ____|_ \_____
+   {tongue}   ||----w |  \ \     \_____ |
+        ||     ||   ||           ||
+'@
+        HasThreeEyes = $false
+    }
+    'hellokitty' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}
+      /\_)o<
+     |      \
+     | O . O|
+      \_____/
+'@
+        HasThreeEyes = $false
+    }
+    'kiss' = @{
+        Body = @'
+     {thoughts}
+      {thoughts}
+             ,;;;;;;;,
+            ;;;;;;;;;;;,
+           ;;;;;'_____;'
+           ;;;(/))))|((\
+           _;;((((((|))))
+          / |_\\\\\\\\\\\\
+     .--~(  \ ~))))))))))))
+    /     \  `\-(((((((((((\\
+    |    | `\   ) |\       /|)
+     |    |  `. _/  \_____/ |
+      |    , `\~            /
+       |    \  \           /
+      | `.   `\|          /
+      |   ~-   `\        /
+       \____~._/~ -_,   (\
+        |-----|\   \    ';;
+       |      | :;;;'     \
+      |  /    |            |
+      |       |            |
+'@
+        HasThreeEyes = $false
+    }
+    'kitty' = @{
+        Body = @'
+     {thoughts}
+      {thoughts}
+       ("`-'  '-/") .___..--' ' "`-._
+         ` *_ *  )    `-.   (      ) .`-.__. `)
+         (_Y_.) ' ._   )   `._` ;  `` -. .-'
+      _.. `--'_..-_/   /--' _ .' ,4
+   ( i l ),-''  ( l i),'  ( ( ! .-'
+'@
+        HasThreeEyes = $false
+    }
+    'koala' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}
+       ___  
+     {~._.~}
+      ( Y )
+     ()~*~()   
+     (_)-(_)
+'@
+        HasThreeEyes = $false
+    }
+    'kosh' = @{
+        Body = @'
+    {thoughts}
+     {thoughts}
+      {thoughts}
+  ___       _____     ___
+ /   \     /    /|   /   \
+|     |   /    / |  |     |
+|     |  /____/  |  |     |
+|     |  |    |  |  |     |
+|     |  | {} | /   |     |
+|     |  |____|/    |     |
+|     |    |==|     |     |
+|      \___________/      |
+|                         |
+|                         |
+'@
+        HasThreeEyes = $false
+    }
+    'lukekoala' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}          .
+       ___   //
+     {~._.~}// 
+      ( Y )K/  
+     ()~*~()   
+     (_)-(_)   
+     Luke    
+     Sywalker
+     koala
+'@
+        HasThreeEyes = $false
+    }
+    'mechandcow' = @{
+        Body = @'
+                                   ,-----.
+                                   |     |
+                                ,--|     |-.
+                         __,----|  |     | |
+                       ,;::     |  `_____' |
+                       `._______|    i^i   |
+                                `----| |---'| .
+                           ,-------._| |== ||//
+                           |       |_|P`.  /'/
+                           `-------' 'Y Y/'/'
+                                     .==\ /_\
+   ^__^                             /   /'|  `i
+   (oo)\_______                   /'   /  |   |
+   (__)\       )/\             /'    /   |   `i
+       ||----w |           ___,;`----'.___L_,-'`\__
+       ||     ||          i_____;----\.____i""\____\
+'@
+        HasThreeEyes = $false
+    }
+    'meow' = @{
+        Body = @'
+  {thoughts}
+   {thoughts} ,   _ ___.--'''`--''//-,-_--_.
+      \`"' ` || \\ \ \\/ / // / ,-\\`,_
+     /'`  \ \ || Y  | \|/ / // / - |__ `-,
+    /@"\  ` \ `\ |  | ||/ // | \/  \  `-._`-,_.,
+   /  _.-. `.-\,___/\ _/|_/_\_\/|_/ |     `-._._)
+   `-'``/  /  |  // \__/\__  /  \__/ \
+        `-'  /-\/  | -|   \__ \   |-' |
+          __/\ / _/ \/ __,-'   ) ,' _|'
+         (((__/(((_.' ((___..-'((__,'
+'@
+        HasThreeEyes = $false
+    }
+    'milk' = @{
+        Body = @'
+ {thoughts}     ____________ 
+  {thoughts}    |__________|
+      /           /\
+     /           /  \
+    /___________/___/|
+    |          |     |
+    |  ==\ /== |     |
+    |   O   O  | \ \ |
+    |     <    |  \ \|
+   /|          |   \ \
+  / |  \_____/ |   / /
+ / /|          |  / /|
+/||\|          | /||\/
+    -------------|   
+        | |    | | 
+       <__/    \__>
+'@
+        HasThreeEyes = $false
+    }
+    'moofasa' = @{
+        Body = @'
+       {thoughts}    ____
+        {thoughts}  /    \
+          | ^__^ |
+          | ({eyes}) |______
+          | (__) |      )\/\
+           \____/|----w |
+                ||     ||
+
+                Moofasa
+'@
+        HasThreeEyes = $false
+    }
+    'moose' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}   \_\_    _/_/
+    {thoughts}      \__/
+           ({eyes})\_______
+           (__)\       )\/\
+            {tongue}   ||----w |
+               ||     ||
+'@
+        HasThreeEyes = $false
+    }
+    'mutilated' = @{
+        Body = @'
+       {thoughts}   \_______
+ v__v   {thoughts}  \   O   )
+ ({eyes})      ||----w |
+ (__)      ||     ||  \/\
+  {tongue}
+'@
+        HasThreeEyes = $false
+    }
+    'ren' = @{
+        Body = @'
+   {thoughts}
+    {thoughts}
+    ____  
+   /# /_\_
+  |  |/o\o\
+  |  \\_/_/
+ / |_   |  
+|  ||\_ ~| 
+|  ||| \/  
+|  |||_    
+ \//  |    
+  ||  |    
+  ||_  \   
+  \_|  o|  
+  /\___/   
+ /  ||||__ 
+    (___)_)
+'@
+        HasThreeEyes = $false
+    }
+    'satanic' = @{
+        Body = @'
+     {thoughts}
+      {thoughts}  (__)  
+         (\/)  
+  /-------\/    
+ / | 666 ||    
+*  ||----||      
+   ~~    ~~
+'@
+        HasThreeEyes = $false
+    }
+    'sheep' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}
+       __     
+      U{eyes}U\.'@@@@@@`.
+      \__/(@@@@@@@@@@)
+           (@@@@@@@@)
+           `YY~~~~YY'
+            ||    ||
+'@
+        HasThreeEyes = $false
+    }
+    'skeleton' = @{
+        Body = @'
+          {thoughts}      (__)      
+           {thoughts}     /{eyes}|  
+            {thoughts}   (_"_)*+++++++++*
+                   //I#\\\\\\\\I\
+                   I[I|I|||||I I `
+                   I`I'///'' I I
+                   I I       I I
+                   ~ ~       ~ ~
+                     Scowleton
+'@
+        HasThreeEyes = $false
+    }
+    'small' = @{
+        Body = @'
+       {thoughts}   ,__,
+        {thoughts}  ({eyes})____
+           (__)    )\
+            {tongue}||--|| *
+'@
+        HasThreeEyes = $false
+    }
+    'sodomized' = @{
+        Body = @'
+      {thoughts}                _
+       {thoughts}              (_)
+        {thoughts}   ^__^       / \
+         {thoughts}  ({eyes})\_____/_\ \
+            (__)\       ) /
+             {tongue} ||----w ((
+                ||     ||>> 
+'@
+        HasThreeEyes = $false
+    }
+    'squirrel' = @{
+        Body = @'
+  {thoughts}
+     {thoughts}
+                  _ _
+       | \__/|  .~    ~.
+       /{eyes} `./      .'
+      {o__,   \    {
+        / .  . )    \
+        `-` '-' \    }
+       .(   _(   )_.'
+      '---.~_ _ _|
+'@
+        HasThreeEyes = $false
+    }
+    'stegosaurus' = @{
+        Body = @'
+{thoughts}                             .       .
+ {thoughts}                           / `.   .' " 
+  {thoughts}                  .---.  <    > <    >  .---.
+   {thoughts}                 |    \  \ - ~ ~ - /  /    |
+         _____          ..-~             ~-..-~
+        |     |   \~~~\.'                    `./~~~/
+       ---------   \__/                        \__/
+      .'  O    \     /               /       \  " 
+     (_____,    `._.'               |         }  \/~~~/
+      `----.          /       }     |        /    \__/
+            `-.      |       /      |       /      `. ,~~|
+                ~-.__|      /_ - ~ ^|      /- _      `..-'   
+                     |     /        |     /     ~-.     `-. _  _  _
+                     |_____|        |_____|         ~ - . _ _ _ _ _>
+'@
+        HasThreeEyes = $false
+    }
+    'stimpy' = @{
+        Body = @'
+  {thoughts}     .    _  .    
+   {thoughts}    |\_|/__/|    
+       / / \/ \  \  
+      /__|O||O|__ \ 
+     |/_ \_/\_/ _\ |  
+     | | (____) | ||  
+     \/\___/\__/  // 
+     (_/         ||
+      |          ||
+      |          ||\   
+       \        //_/  
+        \______//
+       __ || __||
+      (____(____)
+'@
+        HasThreeEyes = $false
+    }
+    'supermilker' = @{
+        Body = @'
+  {thoughts}   ^__^
+   {thoughts}  ({eyes})\_______        ________
+      (__)\       )\/\    |Super |
+       {tongue} ||----W |       |Milker|
+          ||    UDDDDDDDDD|______|
+'@
+        HasThreeEyes = $false
+    }
+    'surgery' = @{
+        Body = @'
+          {thoughts}           \  /
+           {thoughts}           \/
+               (__)    /\
+               ({eyes})   O  O
+               _\/_   //
+         *    (    ) //
+          \  (\\    //
+           \(  \\    )
+            (   \\   )   /\
+  ___[\______/^^^^^^^\__/) o-)__
+ |\__[=======______//________)__\
+ \|_______________//____________|
+     |||      || //||     |||
+     |||      || @.||     |||
+      ||      \/  .\/      ||
+                 . .
+                '.'.`
+
+            COW-OPERATION
+'@
+        HasThreeEyes = $false
+    }
+    'telebears' = @{
+        Body = @'
+      {thoughts}                _
+       {thoughts}              (_)   <-- TeleBEARS
+        {thoughts}   ^__^       / \
+         {thoughts}  ({eyes})\_____/_\ \
+            (__)\  you  ) /
+             {tongue} ||----w ((
+                ||     ||>>
+'@
+        HasThreeEyes = $false
+    }
+    'threeeyes' = @{
+        Body = @'
+        {thoughts}  ^___^
+         {thoughts} ({eyes})\_______
+           (___)\       )\/\
+            {tongue}    ||----w |
+                ||     ||
+'@
+        HasThreeEyes = $true
+    }
+    'turkey' = @{
+        Body = @'
+  {thoughts}                                  ,+*^^*+___+++_
+   {thoughts}                           ,*^^^^              )
+    {thoughts}                       _+*                     ^**+_
+     {thoughts}                    +^       _ _++*+_+++_,         )
+              _+^^*+_    (     ,+*^ ^          \+_        )
+             {       )  (    ,(    ,_+--+--,      ^)      ^\
+            { (@)    } f   ,(  ,+-^ __*_*_  ^^\_   ^\       )
+           {:;-/    (_+*-+^^^^^+*+*<_ _++_)_    )    )      /
+          ( /  (    (        ,___    ^*+_+* )   <    <      \
+           U _/     )    *--<  ) ^\-----++__)   )    )       )
+            (      )  _(^)^^))  )  )\^^^^^))^*+/    /       /
+          (      /  (_))_^)) )  )  ))^^^^^))^^^)__/     +^^
+         (     ,/    (^))^))  )  ) ))^^^^^^^))^^)       _)
+          *+__+*       (_))^)  ) ) ))^^^^^^))^^^^^)____*^
+          \             \_)^)_)) ))^^^^^^^^^^))^^^^)
+           (_             ^\__^^^^^^^^^^^^))^^^^^^^)
+             ^\___            ^\__^^^^^^))^^^^^^^^)\\
+                  ^^^^^\uuu/^^\uuu/^^^^\^\^\^\^\^\^\^\
+                     ___) >____) >___   ^\_\_\_\_\_\_\)
+                    ^^^//\\_^^//\\_^       ^(\_\_\_\)
+                      ^^^ ^^ ^^^ ^
+'@
+        HasThreeEyes = $false
+    }
+    'turtle' = @{
+        Body = @'
+    {thoughts}                                  ___-------___
+     {thoughts}                             _-~~             ~~-_
+      {thoughts}                         _-~                    /~-_
+             /^\__/^\         /~  \                   /    \
+           /|  O|| O|        /      \_______________/        \
+          | |___||__|      /       /                \          \
+          |          \    /      /                    \          \
+          |   (_______) /______/                        \_________ \
+          |         / /         \                      /            \
+           \         \^\\         \                  /               \     /
+             \         ||           \______________/      _-_       //\__//
+               \       ||------_-~~-_ ------------- \ --/~   ~\    || __/
+                 ~-----||====/~     |==================|       |/~~~~~
+                  (_(__/  ./     /                    \_\      \.
+                         (_(___/                         \_____)_)
+'@
+        HasThreeEyes = $false
+    }
+    'tux' = @{
+        Body = @'
+   {thoughts}
+    {thoughts}
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+'@
+        HasThreeEyes = $false
+    }
+    'udder' = @{
+        Body = @'
+  {thoughts}
+   {thoughts}    (__)
+        {eyes}\
+       ('') \---------
+        {tongue}  \           \
+           |          |\
+           ||---(  )_|| *
+           ||    UU  ||
+           ==        ==
+'@
+        HasThreeEyes = $false
+    }
+    'vader' = @{
+        Body = @'
+        {thoughts}    ,-^-.
+         {thoughts}   !oYo!
+          {thoughts} /./=\.\______
+               ##        )\/\
+                ||-----w||
+                ||      ||
+
+               Cowth Vader
+'@
+        HasThreeEyes = $false
+    }
+    'vaderkoala' = @{
+        Body = @'
+   {thoughts}
+    {thoughts}        .
+     .---.  //
+    Y|o o|Y// 
+   /_(i=i)K/ 
+   ~()~*~()~  
+    (_)-(_)   
+
+     Darth
+     Vader
+     koala
+'@
+        HasThreeEyes = $false
+    }
+    'www' = @{
+        Body = @'
+        {thoughts}   ^__^
+         {thoughts}  ({eyes})\_______
+            (__)\       )\/\
+             {tongue} ||--WWW |
+                ||     ||
+'@
+        HasThreeEyes = $false
+    }
+}
+
+# NSFW cowacters list
+$NotSafeForWorkCowacters = @('bongcow', 'sodomized', 'headincow', 'telebears')
