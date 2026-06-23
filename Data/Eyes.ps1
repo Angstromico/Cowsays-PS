e76f9d838c1efb2efcb2f2f7510262893c7151fa
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-23 09:45:55 -0400

    feat(powershell): initialize CowPS module structure and placeholder files

--- Data/Eyes.ps1
+++ Data/Eyes.ps1
@@ -1,1 +1,15 @@
-# Eyes data placeholder
+# Data/Eyes.ps1
+
+$Eyes = @{
+    'default'  = 'oo'
+    'borg'     = '=='
+    'dead'     = 'xx'
+    'greedy'   = '$$'
+    'paranoid' = '@@'
+    'stoned'   = '**'
+    'tired'    = '--'
+    'wired'    = 'OO'
+    'young'    = '..'
+}
+
+$NotSafeForWorkEyes = @('stoned')
